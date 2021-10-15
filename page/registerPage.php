1c0c15b30f0693f45bce70f3e64e91c38c4f1531
rico-sakspari <gitfreder@gmail.com>
2021-10-16 00:21:12 +0800

    Validasi backend (username dan email selesai)

--- page/registerPage.php
+++ page/registerPage.php
@@ -18,11 +18,6 @@ use PHPMailer\PHPMailer\Exception;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
 
-    <script>
-        // form login-register validation
-        const
-    </script>
-
     <title>Login Page</title>
 </head>
 
@@ -104,6 +99,7 @@ use PHPMailer\PHPMailer\Exception;
                                 <label for="username" class="col-lg-3 form-label">Username</label>
                                 <input type="text" class="col-md col-sm-auto form-control" id="email" name="username"
                                        aria-describedby="emailHelp" required>
+                                <div id="email-validation" class="invalid-feedback">Invalid email format</div>
                             </div>
                             <div class="mb-3 row">
                                 <label for="exampleInputPassword" class="col-lg-3 form-label">Password</label>
--- process/verifyEmail.php
+++ process/verifyEmail.php
@@ -16,84 +16,110 @@ if (isset($_POST["register"])) {
     $email = $_POST['email'];
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $inputVerifyStatus = true;
+
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $inputVerifyStatus = false;
         echo
         '<script>
          alert("Email invalid"); window.location = "../page/registerPage.php"
          </script>';
     }
 
+    $query = mysqli_query($con, "SELECT * FROM user_test WHERE username = '$username'") or die(mysqli_error($con));
+    // ini buat ngecek kalo misalnya hasil dari querynya == 0 berarti usernamenya tdk ditemukan
+    if (mysqli_num_rows($query) != 0) {
+        $inputVerifyStatus = false;
+        echo
+        '<script>
+         alert("Username Sudah digunakan"); window.location = "../page/registerPage.php"
+         </script>';
+    }
+
+    $query = mysqli_query($con, "SELECT * FROM user_test WHERE email = '$email'") or die(mysqli_error($con));
+    // ini buat ngecek kalo misalnya hasil dari querynya == 0 berarti emailnya tdk ditemukan
+    if (mysqli_num_rows($query) != 0) {
+        $inputVerifyStatus = false;
+        echo
+        '<script>
+         alert("Email Sudah digunakan"); window.location = "../page/registerPage.php"
+         </script>';
+    }
+
+
     // verify email and username
 
     //Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+    if ($inputVerifyStatus) {
+        $mail = new PHPMailer(true);
 
-    try {
-        //Enable verbose debug output
-        $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;
+        try {
+            //Enable verbose debug output
+            $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;
 
-        //Send using SMTP
-        $mail->isSMTP();
+            //Send using SMTP
+            $mail->isSMTP();
 
-        //Set the SMTP server to send through
-        $mail->Host = 'smtp.gmail.com';
+            //Set the SMTP server to send through
+            $mail->Host = 'smtp.gmail.com';
 
-        //Enable SMTP authentication
-        $mail->SMTPAuth = true;
+            //Enable SMTP authentication
+            $mail->SMTPAuth = true;
 
-        //SMTP username
-        $mail->Username = 'baptistaforpaw@gmail.com';
+            //SMTP username
+            $mail->Username = 'baptistaforpaw@gmail.com';
 
-        //SMTP password
-        $mail->Password = 'BuckyGeming123';
+            //SMTP password
+            $mail->Password = 'BuckyGeming123';
 
-        //Enable TLS encryption;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            //Enable TLS encryption;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 
-        //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-        $mail->Port = 587;
+            //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port = 587;
 
-        //Recipients
-        $mail->setFrom('baptistaforpaw@gmail.com', 'ConnectUs');
+            //Recipients
+            $mail->setFrom('baptistaforpaw@gmail.com', 'ConnectUs');
 
-        //Add a recipient
-        $mail->addAddress($email, $name);
+            //Add a recipient
+            $mail->addAddress($email, $name);
 
-        //Set email format to HTML
-        $mail->isHTML(true);
+            //Set email format to HTML
+            $mail->isHTML(true);
 
-        $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+            $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
 
-        $mail->Subject = 'Email verification';
-        $mail->Body = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
+            $mail->Subject = 'Email verification';
+            $mail->Body = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
 
-        $mail->send();
-        // echo 'Message has been sent';
+            $mail->send();
+            // echo 'Message has been sent';
 
 //        ---------------------------------------
-        $query = mysqli_query($con,
-            "INSERT INTO user_test(name, gender, dateborn, email, username, password, verification_code, verify_at)
+            $query = mysqli_query($con,
+                "INSERT INTO user_test(name, gender, dateborn, email, username, password, verification_code, verify_at)
              VALUES
              (' $name ', ' $gender',' $dateborn ',' $email', ' $username ',' $password ', ' $verification_code ', NULL)")
-        or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
+            or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
 
-        if ($query) {
-            echo
-            '<script>
+            if ($query) {
+                echo
+                '<script>
              alert("Register Success"); window.location = "../page/registerPage.php"
              </script>';
-        } else {
-            echo
-            '<script>
+            } else {
+                echo
+                '<script>
              alert("Register Failed");
              </script>';
+            }
+
+
+            header("Location: ../page/email-verification.php?email=" . $email);
+            exit();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
-
-
-        header("Location: ../page/email-verification.php?email=" . $email);
-        exit();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 ?>
